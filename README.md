50046d537d4faca335ecf38db9074b6a1585d0e9
cfhaak <c.f.haak@posteo.de>
2023-08-28 15:25:58 +0200

    added most files from grocerist, closes #1

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # bv-commentary-parser
-creates tei-files from uploaded docx-files based on standard schema
-docx-documents should be placed in the "docx-files" folder, pushing triggers the automated workflow
+Creates tei-files from docx-files fetched from google-docs.
+Based on [grocerist](https://github.com/grocerist/grocerist-tei).
+
+## usage
+
+* `python scripts/download_docx.py` downloads docx from gdrive, converts it via oxgarage into an XML/TEI Document and saves it into `tmp/source.xml`
+* `python scripts/split.py` splits `tmp/source.xml` in single XML/TEI files and enriches them with baserow-data
+
+## Licensing
+
+All code unless otherwise noted is licensed under the terms [MIT License](https://opensource.org/licenses/MIT).
+
+For licensing information of the data in this repository, please look into the individual folders/files for further information.
---
+++ scripts/download_docx.py
@@ -0,0 +1,43 @@
+import requests
+import shutil
+import os
+import gdown
+from acdh_tei_pyutils.tei import TeiReader
+from config import (
+    CONVERSION_DOMAIN,
+    TMP_DIR,
+    SOURCE_DOCX,
+    SOURCE_DOCX_URL,
+    SOURCE_XML
+)
+
+shutil.rmtree(TMP_DIR, ignore_errors=True)
+os.makedirs(TMP_DIR, exist_ok=True)
+
+print(f"start download from {SOURCE_DOCX_URL}")
+gdown.download(SOURCE_DOCX_URL, SOURCE_DOCX)
+print(f"saved {SOURCE_DOCX}")
+
+
+print(f"posting {SOURCE_DOCX} to {CONVERSION_DOMAIN}")
+headers = {
+    "accept": "application/octet-stream",
+}
+params = {
+    "properties": '<conversions><conversion index="0"><property id="oxgarage.getImages">false</property><property id="oxgarage.getOnlineImages">false</property><property id="oxgarage.lang">en</property><property id="oxgarage.textOnly">true</property><property id="pl.psnc.dl.ege.tei.profileNames">default</property></conversion></conversions>', # noqa
+}
+files = {"fileToConvert": open(SOURCE_DOCX, "rb")}
+response = requests.post(
+    f"{CONVERSION_DOMAIN}ege-webservice/Conversions/docx%3Aapplication%3Avnd.openxmlformats-officedocument.wordprocessingml.document/TEI%3Atext%3Axml", # noqa
+    params=params,
+    headers=headers,
+    files=files,
+)
+data = response.content.decode("utf-8")
+data = data.replace("heading=h.", "heading_h_")
+data = data.replace('xml:id="', 'xml:id="xmlid__')
+print(f"saving result as {SOURCE_XML}")
+with open(SOURCE_XML, "w") as f:
+    f.write(data)
+
+doc = TeiReader(data)
