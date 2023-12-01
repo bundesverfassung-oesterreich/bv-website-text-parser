ba133389876287e52df47676afdc5b44b076a128
cfhaak <c.f.haak@posteo.de>
2023-12-01 17:33:00 +0100

    added two text for the mainpage

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,5 @@
 CONVERSION_DOMAIN = "https://teigarage.tei-c.org/"
 TMP_DIR = "./tmp"
-SOURCE_DOC_GIDS = ["1nUkS-9gDmD7chp8OMplUDQYHBUYnxieP"]
+SOURCE_DOC_GIDS = ["18zdfYuxlUUhRcyo5w5CjcNN3_frpi7XL", "139R2KUkfnNA_DLp4bvMDv53KJOg6GMtS"]
 SOURCE_DOCX_BASE_URL = f"https://drive.google.com/uc?id="
 TEI_DIR = "./tei"
