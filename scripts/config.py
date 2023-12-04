4b2508b634012489d8e9adbedc9d86eb30bf6760
cfhaak <c.f.haak@posteo.de>
2023-12-04 14:11:06 +0100

    removed test

--- scripts/config.py
+++ scripts/config.py
@@ -2,8 +2,7 @@ CONVERSION_DOMAIN = "https://teigarage.tei-c.org/"
 TMP_DIR = "./tmp"
 SOURCE_DOC_GIDS = {
     "18zdfYuxlUUhRcyo5w5CjcNN3_frpi7XL" : "about_project",
-    "139R2KUkfnNA_DLp4bvMDv53KJOg6GMtS" : "about_bvg",
-    #"1MVnSzonMRvNTgefRmZl4wdORIhQgbHtX" : "test"
+    "139R2KUkfnNA_DLp4bvMDv53KJOg6GMtS" : "about_bvg"
 }
 SOURCE_DOCX_BASE_URL = f"https://drive.google.com/uc?id="
 TEI_DIR = "./tei"
